470784514b1d4e027019ca908b4418da318cfb9c
Anderson Paulo <paulinhoanderson.ap@gmail.com>
2023-12-15 15:29:40 -0300

    Adicionando a Computador

---
+++ src/Entities/Computador.java
@@ -0,0 +1,36 @@
+package Entities;
+
+import Entities.*;
+
+public class Computador extends Produto {
+
+    private int ram;
+    private int hd;
+
+    public Computador(String descricao, double preco, int ram, int hd ) {
+        super(descricao, preco);
+        this.ram = ram;
+        this.hd = hd;
+
+    }
+
+
+    @Override
+    public void moreInfo() {
+        System.out.println("Nome: " + getDescricao());
+        System.out.println("Id: " + getId());
+        System.out.printf("Preço: R$ %.2f%n", getPreco());
+
+        if (ram > 0) {
+            System.out.println("RAM: " + ram);
+        } else {
+            System.out.println("Infelixmente não temos essa informação :(");
+        }
+        if (hd > 0){
+            System.out.println("RAM: " + ram);
+        }else {
+            System.out.println("Infelixmente não temos essa informação :(");
+        }
+
+    }
+}
