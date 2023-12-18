f81332c77407cb538b2eddea02110e0d6ab99be8
Anderson Paulo <paulinhoanderson.ap@gmail.com>
2023-12-17 21:06:54 -0300

    LojaController adicinada e corrigida

--- src/Entities/LojaController.java
+++ src/Entities/LojaController.java
@@ -1,10 +1,11 @@
 package Entities;
-
+import Exception.ProductNotFound;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class LojaController {
-    private final Scanner scanner;
-    private final Loja loja;
+    private static Scanner scanner;
+    private static Loja loja;
 
     public LojaController(Scanner scanner) {
         this.loja = new Loja();
@@ -12,13 +13,13 @@ public class LojaController {
     }
 
     public void addProdutoAuto(){
-        loja.addProduto(new Computador("Descktop", 3.500, 8, 500));
-        loja.addProduto(new Computador("Notbook", 4.500, 8, 1000));
-        loja.addProduto(new Computador("Notbook", 3.000, 4, 500));
-        loja.addProduto(new Computador("Descktop", 3.500, 8, 500));
+        loja.addProduto(new Computador("Descktop", 3500, 8 , 500));
+        loja.addProduto(new Computador("Notbook", 4500, 8, 1000));
+        loja.addProduto(new Computador("Notbook", 3000, 4, 500));
+        loja.addProduto(new Computador("Descktop", 3500, 8, 500));
     }
 
-    public void menu() {
+    public static void menu() {
         System.out.println("---------------------------------------");
         System.out.println("------Bem vina a Loja Zerados!!!-------");
         System.out.println("---------------------------------------");
@@ -30,21 +31,78 @@ public class LojaController {
         System.out.println("|     Opção 5 - Sair      |");
 
     }
-    public void processarOpcao(int escolha){
-        int option = scanner.nextInt();
+    public static void processarOpcao(int opcao){
+        int id = 0;
 
-        switch (option) {
+        switch (opcao) {
             case 1:
-                loja.addProduto();
+                System.out.println("------Temos estes produtos em Loja:------");
+                System.out.println("*******      1 - Computador       *******");
+                System.out.println("*******      2 - Periféricos      *******");
+                System.out.print("Qual é o tipo de Produto desejado: ");
+                int escolha2 = scanner.nextInt();
+                try {
+                    switch (escolha2) {
+                        case 1:
+                            scanner.nextLine();
+                            System.out.println("Adicionar o novo computador:");
+                            Produto novoComputador = ComputadorFactory.novoProduto(scanner);
+                            loja.addProduto(novoComputador);
+                            System.out.println("Cadastrado com sucesso!!!");
+                            break;
+                        case 2:
+                            scanner.nextLine();
+                            System.out.println("Adicionar um periférico:");
+                            Produto novoPeriferico = perifericoFactory.novoProduto(scanner);
+                            loja.addProduto(novoPeriferico);
+                            System.out.println("Cadastrado com sucesso!!!");
+                            break;
+                        default:
+                            System.out.println("Opção inválida, por favor tente novamente.");
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println("Entrada inválida. Por favor, insira um dado válido!");
+                    scanner.nextLine();
+                }
                 break;
             case 2:
                 loja.listStock();
                 break;
             case 3:
-                loja.comprarProdutos();
+                System.out.println("Qual o produto que você quer comprar?");
+                System.out.print("Digite o ID do produto: ");
+                id = scanner.nextInt();
+                System.out.println("_____________________________________");
+                try {
+                    if (loja.buscarPorId(id) != null) {
+                        scanner.nextLine();
+                        System.out.print("Deseja comprar este produto? (S - Sim | N - Não): ");
+                        char resposta = scanner.nextLine().toUpperCase().charAt(0);
+                        if (resposta == 'S') {
+                            loja.comprarProduto(id);
+                        } else {
+                            System.out.println("Compra cancelada!");
+                        }
+                    }
+                } catch (InputMismatchException | ProductNotFound e) {
+                    System.out.println("Entrada inválida. Por favor, insira um número válido.");
+                }
                 break;
             case 4:
-                loja.listCarrinho();
+                System.out.println("______________________________________________");
+                System.out.print("Digite o id do produto que você deseja remover: ");
+                id = scanner.nextInt();
+                try {
+                    scanner.nextLine();
+                    Produto produtoId = loja.deleteProdutoById(id);
+                    System.out.println("Este será o produto Deletado!");
+                    produtoId.moreInfo();
+                    System.out.println("         !!!SUCESSO!!!        ");
+                    System.out.println("--- O produto foi Removido ---");
+                } catch (InputMismatchException | ProductNotFound e) {
+                    System.out.println("Entrada inválida. Por favor, insira um número válido.");
+                    scanner.nextLine();
+                }
                 break;
             case 5:
                 System.out.println("------Obrigado pela preferência!-------");
@@ -53,7 +111,7 @@ public class LojaController {
                 System.exit(0);
             default:
                 System.out.println("Opção Inválida!");
-                menu();
         }
     }
+
 }
